b7cf57b06c9d277ad30ff912a0597a39cf558db8
himani bhardwaj <himani.bhardwaj1@ibm.com>
2023-09-08 13:07:35 +0530

    created Register and Login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import {Home} from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <Product/>
+    <Login/>
   );
 }
 
